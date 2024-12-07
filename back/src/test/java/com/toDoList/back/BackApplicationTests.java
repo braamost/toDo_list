369aa18c6c1b2d148a6fb8dabd0d5b003c6d7827
braa mostafa <braamostafa1@gmail.com>
2024-12-07 23:35:27 +0200

    back

---
+++ back/src/test/java/com/toDoList/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toDoList.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
