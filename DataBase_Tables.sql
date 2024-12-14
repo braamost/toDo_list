84b7aecb9b3ea40c19e10e864e2e861364f00999
braa mostafa <braamostafa1@gmail.com>
2024-12-14 04:49:09 +0200

    .

--- DataBase_Tables.sql
+++ DataBase_Tables.sql
@@ -1,7 +1,14 @@
 CREATE DATABASE IF NOT EXISTS `todo_list`;
 USE `todo_list`;
 
+-- Drop existing tables if they exist
+DROP TABLE IF EXISTS `TodoLists`;
+DROP TABLE IF EXISTS `Categories`;
+DROP TABLE IF EXISTS `Users`;
+
 CREATE TABLE Users (
+	first_name VARCHAR(50) NOT NULL,  
+    second_name VARCHAR(50) NOT NULL,  
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE COLLATE utf8_bin,  -- Case-sensitive collation
     password_hash VARCHAR(255) NOT NULL,
