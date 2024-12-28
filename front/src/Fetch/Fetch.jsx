eadff71684e8028c66a6264ed19077a97dfeff9b
braa mostafa <braamostafa1@gmail.com>
2024-12-28 02:01:34 +0200

    added delete category

--- front/src/Fetch/Fetch.jsx
+++ front/src/Fetch/Fetch.jsx
@@ -4,6 +4,7 @@ export async function fetchData(user, setUser) {
   try {
     console.log(`Fetching data for user: ${user.username}`);
     const response = await axios.get(`http://localhost:8080/api/categories/${user.username}`);
+    console.log("categories: ", response.data);
     setUser(prevUser => ({
       ...prevUser,
       categories: response.data
--- front/src/MainPage/Components/ContentSection.jsx
+++ front/src/MainPage/Components/ContentSection.jsx
@@ -1,6 +1,6 @@
-import React, { useState,useContext } from 'react';
-import MyTasks from './Table.jsx';
-import { Datacontext } from '../../main';
+import React, { useState, useContext } from "react";
+import MyTasks from "./Table.jsx";
+import { Datacontext } from "../../main";
 import { fetchData } from "../../Fetch/Fetch";
 import axios from "axios";
 import {
@@ -20,8 +20,7 @@ import {
 import TaskAdding from "./composeTask";
 import { deleteCategory } from "../../Delete/DeleteCategories";
 const ContentSection = ({ title, category, messages = [] }) => {
-
-  const {user,setUser} =useContext(Datacontext);
+  const { user, setUser } = useContext(Datacontext);
   const handleDelete = async (e) => {
     try {
       console.log("Deleting category:", title);
@@ -32,17 +31,41 @@ const ContentSection = ({ title, category, messages = [] }) => {
     } catch (error) {
       console.error("Error in handleDelete:", error);
       // You might want to show an error message to the user here
-    }};
+    }
+  };
   return (
     <>
-
-    <div>
-      <div className='flex flex-col items-center text-gray-500 font-medium text-sm justify-between space-x-2'>
-      <MyTasks></MyTasks>
-      
-      
+      <div>
+        <div className="flex flex-col items-center text-gray-500 font-medium text-sm justify-between space-x-2">
+          <h3 className="text-2xl font-bold text-gray-800 mb-6">{title}</h3>
+          <MyTasks/>
+          <nav className="mt-8 space-y-2">
+            <div className="min-w-full min-h-full ">
+              <TaskAdding category={category}></TaskAdding>
+              <button
+                className="
+                   bg-red-500
+                   hover:bg-red-600
+                   text-white
+                   p-3
+                   rounded-full
+                   shadow-lg
+                   transition
+                   duration-300
+                   ease-in-out
+                   hover:scale-110
+                   fixed
+                   left-80
+                   bottom-20
+                 "
+                onClick={handleDelete}
+              >
+                <TrashIcon className="w-full h-full" />
+              </button>
+            </div>
+          </nav>
+        </div>
       </div>
-    </div>
     </>
   );
 };
